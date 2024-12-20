ed26536580e9ab06bf31aa56f6083a0bfefd1d4e
Guguloth Rambabu Nayak <gnayak@deloitte.com>
2024-12-20 15:03:03 +0530

    initail commit

---
+++ C#/Sample Applications/Learnings/MyConsoleApp/Calculate.cs
@@ -0,0 +1,13 @@
+namespace Sample_Console_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x =2;
+            int y =3;
+            int Sum = x+y;
+            Console.WriteLine("Hello, Roja!, Sum of 2+3 is " + Sum);
+        }
+    }
+}
